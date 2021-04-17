6fffbfc1ee782ae30fa48d1ed587f53c91115678
rj21dart <rajsen89611@gmail.com>
2021-04-17 22:08:11 +0530

    first_commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,37 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `notes`,
+    //     path: `${__dirname}/src/notes/`,
+    //   },
+    // },
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    }
+    
+  ],
+  siteMetadata: {
+    title: 'Rj@ Warrior',
+    description: 'Rj@dart portfolio',
+    copyright: 'This website is copyright 2021 Rj Warrior',
+    contact: 'rj@dart.com'
+  }
 }
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex atque illo non nobis vel, eos quam fugiat perferendis ipsum aut corrupti quaerat quo eum quibusdam, laboriosam iusto dicta consectetur veniam!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex atque illo non nobis vel, eos quam fugiat perferendis ipsum aut corrupti quaerat quo eum quibusdam, laboriosam iusto dicta consectetur veniam!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex atque illo non nobis vel, eos quam fugiat perferendis ipsum aut corrupti quaerat quo eum quibusdam, laboriosam iusto dicta consectetur veniam!</p>
+            </div>
+        </Layout>
+    )
+}
